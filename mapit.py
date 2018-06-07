71bd1cd365cf3fec035a694c839884dcd9b33b78
Avinash Bharti <avinashbharti97@gmail.com>
2018-06-08 00:03:58 +0530

    instructions added

--- mapit.py
+++ mapit.py
@@ -2,6 +2,19 @@
 
 #mapit.py -reads address from command line and open that address in google map
 
+# -------install dependencies-------------
+# $ pip install pyperclip
+# $ sudo apt-get install xsel
+# $ sudo apt-get install xclip
+
+#--------how to use----------
+#Method1
+# -copy the desired address
+# run the script
+
+#method2
+# pass the address in command line after the file name
+
 import webbrowser, sys, pyperclip
 
 if len(sys.argv)>1:
